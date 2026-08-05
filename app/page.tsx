64409eed1a42d885cc36c32436f5a18942d91da0
rifat-two-eight <rifat28.dev@gmail.com>
2026-08-05 15:23:04 +0600

    one section removed

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Hero />
       <Philosophy />
       <AppearanceManagement />
-      <SpecializedServices />
+      {/* <SpecializedServices /> */}
       <Institution />
       <Footer />
     </div>
